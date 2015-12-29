c66efe4198570c23d677b4dacc2fcee96e6b49fa
francescoagati1975 <francescoagati1975@gmail.com>
2015-12-30 00:34:38 +0100

    exploring

--- test/Test.hx
+++ test/Test.hx
@@ -1,10 +1,36 @@
 import js.vue.Vue;
-import vue.macro.Builder;
-import TestComponent;
+//import vue.macro.Builder;
+//import TestComponent;
+
+class Component extends Vue<Dynamic> {
+
+  function click(e) {
+    trace(this);
+  }
+
+  public function new(config) {
+    config.methods = untyped {};
+    config.methods.click = click;
+    super(config);
+  }
+
+}
+
 
 class Test {
 
   static function main() {
-
+    new Component(untyped {
+      el: '#demo',
+      template:'
+        <div id="demo" @click="click">
+          <p>{{message}}</p>
+          <input v-model="message">
+        </div>
+      ',
+      data: {
+        message: 'Hello Vue.js!'
+      }
+    });
   }
 }
--- test/TestComponent.hx
+++ test/TestComponent.hx
@@ -7,15 +7,13 @@ typedef Data = {a:Int};
 @:template('ddeeeeddd')
 class TestComponent extends vue.macro.Component<Data> {
 
-
-
   @:bind function click(e) {}
   @:computed function sum() return untyped vue.a + 1;
   @:watch("a.b.c",{immediate:true}) function pippa(old,new_value) {}
 
   public inline function new(data) {
     super(data);
-    //init(data);
+    initialize(data);
   }
 
 
